0baf56627c3d23c3558c1e941f37677e40fa38b6
sooof <sooof@me.com>
2022-02-09 20:41:53 -0800

    add dog-router.js adopters-router.js

---
+++ api/dogs/dogs-router.js
@@ -0,0 +1,17 @@
+const router = require('express').Router()
+const Dog = require('./dogs/dogs-model');
+// DOGS ENDPOINTS
+router.get('/api/dogs', (req, res) => {
+    Dog.find()
+      .then(dogs => {
+        res.status(200).json(dogs);
+      })
+      .catch(error => {
+        console.log(error);
+        res.status(500).json({
+          message: 'Error retrieving the dogs',
+        });
+      });
+  });
+
+module.exports = router
